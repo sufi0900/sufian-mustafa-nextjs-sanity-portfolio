3cf73d2dffebb846bbcd5616b63c5b56ce1b545d
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2023-11-11 20:18:39 +0500

    New Commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,10 +8,17 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 import { Analytics } from "@vercel/analytics/react";
 import "./body.dark-mode.css";
+import Head from "next/head";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="k0JxDLTBR6jAV0eTVc6UxwWSEuoyBpz1c4k0zI7mKL4"
+        />
+      </Head>
       <body>
         <ThemeProvider theme={theme}>
           <CssBaseline />
