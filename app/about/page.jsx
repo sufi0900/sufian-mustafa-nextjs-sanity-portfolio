b8b9f2950858e7c329ddb4adf0f4c9cf02b3fa8c
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2023-11-11 16:24:01 +0500

    New Commit

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -40,6 +40,7 @@ const page = () => {
   }
   return (
     <div>
+      <br />
       <Head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
