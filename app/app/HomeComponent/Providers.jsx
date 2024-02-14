804e6c39dbfe7f1af076ced0b8e4004cfd296ad9
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2024-02-14 21:25:36 +0500

    New Commit

--- app/app/HomeComponent/Providers.jsx
+++
@@ -1,7 +0,0 @@
-// "use client";
-// import React from "react";
-// import { ThemeProvider } from "next-themes";
-
-// export default function Providers({ children }) {
-//   return <ThemeProvider>{children}</ThemeProvider>;
-// }
--- app/app/blogs2/Client.js
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-import Server from "./Server";
-const Client = () => {
-  return <Server />;
-};
-
-export default Client;
